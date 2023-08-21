ad503beeb3dd697126b50c1554c84f722657d752
Zestgeek-22 <zestgeek-22@Zestgeek-22s-MacBook-Air.local>
2023-08-21 13:29:26 +0530

    common and dummy  components created.

---
+++ ecommerce/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+
+const Footer:FC=()=>{
+ return (<>
+     <div className=" block w-full bg-black text-white h-9">
+        <h4 className="text">© 2023 <span className="text-yellow-700 mr-3 ml-2">EMarket</span>All   rights reserved </h4>
+     </div>
+      </>)
+}
+export default Footer;
--- ecommerce/src/pages/Home.tsx
+++ ecommerce/src/pages/Home.tsx
@@ -1,19 +1,22 @@
 import { FC } from "react"
+import Footer from "../components/Footer";
 
 const HomePage:FC=()=>{
 
     return(<>
         <nav className="flex sm:justify-center space-x-4">
-            <img src="../assets/ecommerce-logo.png" alt="logo image"/>
+            <img src="../assets/ecommerce-low-resolution-logo-color-on-transparent-background.png" className="w-full" alt="logo image"/>
   {[
     ['Home', '/dashboard'],
     ['Products', '/team'],
-    ['Projects', '/projects'],
+    ['Categories', '/projects'],
     ['Login/SignUP', '/reports'],
   ].map(([title, url]) => (
     <a href={url} className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">{title}</a>
   ))}
 </nav>
+       
+       <Footer/>
         </>)
 }
 export default HomePage;
